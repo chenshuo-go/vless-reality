a4ffbed88154a10636d3d576debcdb52151c8509
chenshuo-go <148471749+chenshuo-go@users.noreply.github.com>
2023-10-20 00:24:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ if [ $# -ge 1 ]; then
     # 第3个参数是域名
     domain=${3}
     if [[ -z $domain ]]; then
-      domain="app.lootbear.com"
+      domain="csgo-skin-changer.pl"
     fi
 
     # 第4个参数是UUID
@@ -258,7 +258,7 @@ fi
 if [[ -z $domain ]]; then
   echo -e "请输入一个 ${magenta}合适的域名${none} Input the domain"
   read -p "(例如: learn.microsoft.com): " domain
-  [ -z "$domain" ] && domain="app.lootbear.com"
+  [ -z "$domain" ] && domain="csgo-skin-changer.pl"
 
   echo
   echo
