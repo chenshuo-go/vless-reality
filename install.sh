f8588b7249772f42ba26441cfd05d398a47abc7b
chenshuo-go <148471749+chenshuo-go@users.noreply.github.com>
2024-12-25 16:11:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ if [ $# -ge 1 ]; then
     # 第3个参数是域名
     domain=${3}
     if [[ -z $domain ]]; then
-      domain="www.broadcom.com"
+      domain="www.trellix.com"
     fi
 
     # 第4个参数是UUID
@@ -258,7 +258,7 @@ fi
 if [[ -z $domain ]]; then
   echo -e "请输入一个 ${magenta}合适的域名${none} Input the domain"
   read -p "(例如: learn.microsoft.com): " domain
-  [ -z "$domain" ] && domain="www.broadcom.com"
+  [ -z "$domain" ] && domain="www.trellix.com"
 
   echo
   echo
